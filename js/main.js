09d0c157811ec719f909c98cfa704b3482fd725c
Mahmoud Albasndi <mahmoud.albasndi@hotmail.com>
2022-05-23 00:56:44 +0300

    create website

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//Function header to add class
+let nav = document.getElementById("nav__bar");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 50) {
+    nav.classList.add("box-sh-nav");
+  } else {
+    nav.classList.remove("box-sh-nav");
+  }
+});
+console.log(nav);
